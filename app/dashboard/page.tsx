3966f105612a8dbc8b94f63ddefb851a92d70203
Osvaldo R.Moreno Prieto <osvaldo020213.com>
2024-02-25 01:33:10 -0500

    fix/invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,8 +7,12 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData(); 
-  
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
